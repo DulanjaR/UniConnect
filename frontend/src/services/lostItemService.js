634709633947990f62aefd3f3b43e4345ac046d8
Dulanja <dula.tr14@gmail.com>
2026-04-11 10:42:21 +0530

    lost and found updates

--- frontend/src/services/lostItemService.js
+++ frontend/src/services/lostItemService.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/lost-items";
+const API = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 export const getItems = async (params = {}) => {
-  return axios.get(API, { params });
+  const token = localStorage.getItem('token');
+  const headers = token ? { Authorization: `Bearer ${token}` } : {};
+  return axios.get(`${API}/lost-items`, { params, headers });
 };
 
 export const createItem = async (data, token) => {
-  return axios.post(API, data, {
+  return axios.post(`${API}/lost-items`, data, {
     headers: {
       Authorization: `Bearer ${token}`,
       "Content-Type": "multipart/form-data",
